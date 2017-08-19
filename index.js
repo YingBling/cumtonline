40b3828b0980476596abd26b558ca284db25efd0
Bling <1135536203@qq.com>
2017-08-19 15:32:17 +0800

    导航栏
    导航栏下滑自动隐藏，上滑显示

---
+++ index.js
@@ -0,0 +1,41 @@
+// var Ul=document.getElementById("Ul");
+
+// header.style.display="block";
+// document.getElementById("fOcus1").onclick=function(){
+//     header.style.display="none";
+// }
+// document.getElementById("fOcus2").onclick=function(){
+//     header.style.display="none";
+// }
+// document.getElementById("fOcus3").onclick=function(){
+//     header.style.display="none";
+// }
+// document.getElementById("fOcus4").onclick=function(){
+//     header.style.display="none";
+// }
+// document.getElementById("fOcus5").onclick=function(){
+//     header.style.display="none";
+// }
+
+var new_scroll_position = 0;
+    var last_scroll_position;
+    var header = document.getElementById("header");
+
+    window.addEventListener('scroll', function(e) {
+      last_scroll_position = window.scrollY;
+
+      // Scrolling down
+      if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+        // header.removeClass('slideDown').addClass('slideUp');
+        header.classList.remove("slideDown");
+        header.classList.add("slideUp");
+
+      // Scrolling up
+      } else if (new_scroll_position > last_scroll_position) {
+        // header.removeClass('slideUp').addClass('slideDown');
+        header.classList.remove("slideUp");
+        header.classList.add("slideDown");
+      }
+
+      new_scroll_position = last_scroll_position;
+    });
